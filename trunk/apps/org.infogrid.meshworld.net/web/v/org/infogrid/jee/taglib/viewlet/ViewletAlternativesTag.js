4bb6ca1d2067f83f1ff760b01230d18d68df0268
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2008-03-26 20:41:42 +0000

    Incorrectly tagged, removing again

--- trunk/apps/org.infogrid.meshworld.net/web/v/org/infogrid/jee/taglib/viewlet/ViewletAlternativesTag.js
+++
@@ -1,8 +0,0 @@
-function toggle_viewlet_alternatives() {
-    var div = document.getElementById( 'org-infogrid-jee-taglib-viewlet-ViewletAlternativesTag' );
-    if( div.getAttribute( 'class' ) == 'org-infogrid-jee-taglib-viewlet-ViewletAlternativesTag' ) {
-        div.setAttribute( 'class', 'org-infogrid-jee-taglib-viewlet-ViewletAlternativesTag-expanded' );
-    } else {
-        div.setAttribute( 'class', 'org-infogrid-jee-taglib-viewlet-ViewletAlternativesTag' );
-    }
-}
--- trunk/tools/cleanall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for m in {apps,tests,modules}/*/ ; do
-	echo Cleaning module $m
-	ant -f "$m"build.xml -Dno.deps=1 clean > /dev/null
-done;
