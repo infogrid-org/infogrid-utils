8af1ec5f23e76bba19db65e75fd75c80eec49371
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2008-05-12 05:41:06 +0000

    tests work again

--- tests/org.infogrid.kernel.net.TEST.xpriso/src/org/infogrid/kernel/net/TEST/xpriso/AllTests.java
+++ tests/org.infogrid.kernel.net.TEST.xpriso/src/org/infogrid/kernel/net/TEST/xpriso/AllTests.java
@@ -50,7 +50,7 @@ public class AllTests
                 // new TestSpec( XprisoTest11.class ), // FAILS. Not sure it is an error, though. FIXME?
                 new TestSpec( XprisoTest12.class ),
                 new TestSpec( XprisoTest13.class ),
-//                new TestSpec( XprisoTest14.class )
+                new TestSpec( XprisoTest14.class )
         };
 
         runTests( tests );
