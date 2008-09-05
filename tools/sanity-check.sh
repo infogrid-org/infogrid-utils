4a0a928fa4ea0840642c54a8b93f1adc08d812f3
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2008-09-05 21:29:25 +0000

    added to tools/sanity-check.sh added tools/add-project-to-svn fixed copyright statements in tools/*

--- tools/sanity-check.sh
+++ tools/sanity-check.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
+# This file is part of InfoGrid(tm). You may not use this file except in
+# compliance with the InfoGrid license. The InfoGrid license and important
+# disclaimers are contained in the file LICENSE.InfoGrid.txt that you should
+# have received with InfoGrid. If you have not received LICENSE.InfoGrid.txt
+# or you do not consent to all aspects of the license and the disclaimers,
+# no license is granted; do not use this file.
+#
+# For more information about InfoGrid go to http://infogrid.org/
+#
+# Copyright 1998-2008 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
+# All rights reserved.
+#
+# (end of header)
 #
 # Performs some sanity checks for common errors. Should be run prior to
 # check-in
 
+FLAGS="-i";
+
 echo '** Checking that no funny paths exist **'
-grep '\.\./\.\./\.\.' {modules*,apps*,tests*}/*/nbproject/project.properties
+grep ${FLAGS} '\.\./\.\./\.\.' {modules*,apps*,tests*}/*/nbproject/project.properties
 
 echo '** Checking that the Vendor is set right'
-grep application.vendor {modules*,apps*,tests*}/*/nbproject/project.properties | grep -v InfoGrid.org
+grep ${FLAGS} application.vendor {modules*,apps*,tests*}/*/nbproject/project.properties | grep -v InfoGrid.org
+
+for pattern in "$@"; do
+	echo '** Checking that pattern' ${pattern} 'does not exist.'
+	find . -type f -and -not -path '*/.svn/*' -exec grep ${FLAGS} -H ${pattern} {} \;
+done
