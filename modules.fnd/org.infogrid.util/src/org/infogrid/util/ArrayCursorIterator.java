9388a958b7542d5154abf5155a11fbb25178a9dd
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2009-04-09 03:03:27 +0000

    minor

--- modules.fnd/org.infogrid.util/src/org/infogrid/util/ArrayCursorIterator.java
+++ modules.fnd/org.infogrid.util/src/org/infogrid/util/ArrayCursorIterator.java
@@ -8,7 +8,7 @@
 // 
 // For more information about InfoGrid go to http://infogrid.org/
 //
-// Copyright 1998-2008 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
+// Copyright 1998-2009 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
 // All rights reserved.
 //
 
@@ -146,6 +146,8 @@ public class ArrayCursorIterator<E>
      */
     @Override
     public E peekNext()
+            throws
+                NoSuchElementException
     {
         if( thePosition >= theLowerBound && thePosition < theUpperBound ) {
             return theArray[ thePosition ];
@@ -162,6 +164,8 @@ public class ArrayCursorIterator<E>
      */
     @Override
     public E peekPrevious()
+            throws
+                NoSuchElementException
     {
         if( thePosition > theLowerBound && thePosition <= theUpperBound ) {
             return theArray[ thePosition-1 ];
