af11d6edaf5b71b782e6dcefb54fb2c1eee77720
build <build@infogrid.org>
2009-06-09 22:06:52 +0000

    Renamed Tomcat test harness path names Factored out ant library into functions and data Better tomcat script

--- tools/tomcat.sh
+++ tools/tomcat.sh
@@ -37,6 +37,7 @@ help=1;
 ANTFLAGS=;
 CONFIG=;
 project=;
+path=;
 
 for arg in $*; do
 	if [ "${ANTFLAGS}" = 'ANTFLAGS' ]; then
@@ -59,6 +60,8 @@ for arg in $*; do
 		ANTFLAGS='ANTFLAGS';
 	elif [ "$project" = '' ]; then
 		project="${arg}";
+	elif [ "$path" = '' ]; then
+		project="${path}";
 	else
 		echo "ERROR: Cannot work on two projects at a time: ${project} vs. ${arg}."
 		exit 1;
@@ -68,7 +71,9 @@ done
 
 if [ "${help}" = 0 -o "${ANTFLAGS}" = 'ANTFLAGS' -o "${CONFIG}" = 'CONFIG' ]; then
 	echo Synopsis:
-	echo "    ${script} [-v][-h][-n] \(-deploy|-undeploy\) [-antflags <flags>] -c <config file> <project>"
+	echo "    ${script} [-v][-n] -deploy [-antflags <flags>] -c <config file> <project> [<path>]"
+	echo "    ${script} [-v][-n] -undeploy [-antflags <flags>] -c <config file> <project> [<path>]"
+	echo "    ${script} -h"
 	echo "        -v: verbose output"
 	echo "        -h: this help"
 	echo "        -n: do not execute, only print"
@@ -77,6 +82,7 @@ if [ "${help}" = 0 -o "${ANTFLAGS}" = 'ANTFLAGS' -o "${CONFIG}" = 'CONFIG' ]; th
 	echo "        -antflags <flags>: pass flags to ant invocation"
 	echo "        -c <configfile>: use configuration file"
 	echo "        <project>: relative path to project's top directory"
+	echo "        <path>: if given, context path for deployment; defaults to project name"
 	exit 1;
 fi
 
@@ -113,6 +119,9 @@ if [ "${do_nothing}" = 0 ]; then
 		/bin/echo -n " undeploy"
 	fi
 	/bin/echo -n : ${project} with configuration ${CONFIG}
+	if [ ! -z "${path}" ]; then
+		/bin/echo -n " at path ${path}"
+	fi
 	if [ "${verbose}" = 0 ]; then
 		/bin/echo -n " (verbose)"
 	fi
@@ -130,11 +139,17 @@ function stop
 
 if [ "${do_undeploy}" = 0 ]; then
 	cmd="ant -f ${project}/build.xml ${ANTFLAGS} tomcat-undeploy"
+        if [ ! -z "${path}" ]; then
+		cmd="${cmd} -Dwebapp.path=${path}"
+	fi
 	${cmd}
 fi
 
 if [ "${do_deploy}" = 0 ]; then
 	cmd="ant -f ${project}/build.xml ${ANTFLAGS} tomcat-deploy"
+        if [ ! -z "${path}" ]; then
+		cmd="${cmd} -Dwebapp.path=${path}"
+	fi
 	${cmd}
 fi
 	
