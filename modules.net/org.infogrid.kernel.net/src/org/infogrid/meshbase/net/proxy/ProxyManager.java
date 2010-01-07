46667682b3ab9bf451b3e6df54603985f0db8396
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2010-01-07 19:29:52 +0000

    Appears we are missing a flush when the ProxyManager dies

--- modules.net/org.infogrid.kernel.net/src/org/infogrid/meshbase/net/proxy/ProxyManager.java
+++ modules.net/org.infogrid.kernel.net/src/org/infogrid/meshbase/net/proxy/ProxyManager.java
@@ -88,6 +88,7 @@ public class ProxyManager
         }
         for( Proxy p : temp ) {
             p.die( isPermanent );
+            factoryCreatedObjectUpdated( p );
             if( isPermanent ) {
                 remove( p.getPartnerMeshBaseIdentifier() );
             }
