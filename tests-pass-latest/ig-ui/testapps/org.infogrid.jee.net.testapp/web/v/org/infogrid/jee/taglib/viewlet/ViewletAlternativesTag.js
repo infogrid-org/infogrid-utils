5d54ea511210bf80be84386ebb9858bdf5830444
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2010-05-12 22:56:12 +0000

    SVN operator error, sorry

--- tests-pass-latest/ig-ui/testapps/org.infogrid.jee.net.testapp/web/v/org/infogrid/jee/taglib/viewlet/ViewletAlternativesTag.js
+++
@@ -1,8 +0,0 @@
-function toggle_viewlet_alternatives( id ) {
-    var div = document.getElementById( id );
-    if( div.className == 'org-infogrid-jee-taglib-viewlet-ViewletAlternativesTag' ) {
-        div.className = 'org-infogrid-jee-taglib-viewlet-ViewletAlternativesTag-expanded';
-    } else {
-        div.className = 'org-infogrid-jee-taglib-viewlet-ViewletAlternativesTag';
-    }
-}
--- tests-pass-latest/ig-ui/testapps/org.infogrid.jee.net.testapp/web/v/org/infogrid/jee/viewlet/propertysheet/PropertySheetViewlet/audit.jsp
+++
@@ -1,5 +0,0 @@
-<div class="audit">
- <p>Created: <mesh:timeCreated meshObjectName="Subject" /></p>
- <p>Updated: <mesh:timeUpdated meshObjectName="Subject" /></p>
- <p>Last&nbsp;read: <mesh:timeRead meshObjectName="Subject" /></p>
-</div>
