db20de52934f42708dfa68882a3a5b7b3a5a3e3a
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2010-06-08 20:20:39 +0000

    minor

--- ig-ui/modules/org.infogrid.jee.viewlet/src/org/infogrid/jee/taglib/logic/IfTag.java
+++ ig-ui/modules/org.infogrid.jee.viewlet/src/org/infogrid/jee/taglib/logic/IfTag.java
@@ -8,7 +8,7 @@
 // 
 // For more information about InfoGrid go to http://infogrid.org/
 //
-// Copyright 1998-2008 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
+// Copyright 1998-2010 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
 // All rights reserved.
 //
 
@@ -62,5 +62,4 @@ public class IfTag
         }
         return ret;
     }
-
 }
