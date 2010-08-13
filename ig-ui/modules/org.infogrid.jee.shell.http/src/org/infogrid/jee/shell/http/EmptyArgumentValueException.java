2d3dff7e3ffb6cdb789b87edb3c8600a4e80bc82
hudson <build@infogrid.org>
2010-08-13 06:39:21 +0000

    svn merge -r 1045:1048 http://svn.infogrid.org/infogrid/trunk

---
+++ ig-ui/modules/org.infogrid.jee.shell.http/src/org/infogrid/jee/shell/http/EmptyArgumentValueException.java
@@ -0,0 +1,63 @@
+//
+// This file is part of InfoGrid(tm). You may not use this file except in
+// compliance with the InfoGrid license. The InfoGrid license and important
+// disclaimers are contained in the file LICENSE.InfoGrid.txt that you should
+// have received with InfoGrid. If you have not received LICENSE.InfoGrid.txt
+// or you do not consent to all aspects of the license and the disclaimers,
+// no license is granted; do not use this file.
+//
+// For more information about InfoGrid go to http://infogrid.org/
+//
+// Copyright 1998-2010 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
+// All rights reserved.
+//
+
+package org.infogrid.jee.shell.http;
+
+import org.infogrid.util.AbstractLocalizedException;
+
+/**
+ * Thrown if an operation is invoked with an argument that does not have a value.
+ */
+public class EmptyArgumentValueException
+        extends
+            AbstractLocalizedException
+{
+    private static final long serialVersionUID = 1L; // helps with serialization
+
+    /**
+     * Constructor.
+     *
+     * @param argName name of the argument without a value
+     */
+    public EmptyArgumentValueException(
+            String argName )
+    {
+        theArgumentName = argName;
+    }
+
+    /**
+     * Obtain the name of the missing argument.
+     *
+     * @return name of the argument
+     */
+    public String getArgumentName()
+    {
+        return theArgumentName;
+    }
+
+    /**
+     * Obtain resource parameters for the internationalization.
+     *
+     * @return the resource parameters
+     */
+    public Object [] getLocalizationParameters()
+    {
+        return new Object[] { theArgumentName };
+    }
+
+    /**
+     * Name of the missing argument.
+     */
+    protected String theArgumentName;
+}
