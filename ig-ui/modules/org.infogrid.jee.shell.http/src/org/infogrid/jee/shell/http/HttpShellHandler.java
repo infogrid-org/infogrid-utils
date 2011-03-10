39a4e6a41834c7948df31fde6fbd4b6f31683316
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2011-03-10 00:45:07 +0000

    augmented HttpShellHandler to have invocable method before, after, at the beginning and at the end of Transactions

--- ig-ui/modules/org.infogrid.jee.shell.http/src/org/infogrid/jee/shell/http/HttpShellHandler.java
+++ ig-ui/modules/org.infogrid.jee.shell.http/src/org/infogrid/jee/shell/http/HttpShellHandler.java
@@ -18,11 +18,13 @@ import java.util.Map;
 import org.infogrid.mesh.MeshObject;
 import org.infogrid.meshbase.MeshBase;
 import org.infogrid.meshbase.transaction.OnDemandTransaction;
+import org.infogrid.meshbase.transaction.TransactionException;
 import org.infogrid.util.SmartFactory;
 import org.infogrid.util.http.SaneRequest;
 
 /**
- * Implemented by classes that can be invoked after the HttpShell is done with its processing.
+ * Implemented by classes that would like to take action before, during, or after
+ * regular HttpShell processing.
  */
 public interface HttpShellHandler
 {
@@ -32,21 +34,69 @@ public interface HttpShellHandler
      * @return the name1
      */
     public String getName();
-    
+
     /**
-     * The main execution method.
+     * Invoked by the HttpShell before any other processing takes place.
+     *
+     * @param request the incoming request
+     * @param defaultMeshBase the default MeshBase to use
+     * @throws HttpShellException a problem occurred, check cause for details
+     */
+    public void beforeTransactionStart(
+            SaneRequest                                     request,
+            MeshBase                                        defaultMeshBase )
+        throws
+            HttpShellException;
+
+    /**
+     * Invoked by the HttpShell after any Transaction was created, but before any
+     * processing within the transaction takes place.
+     *
+     * @param request the incoming request
+     * @param txs the Transactions used by this invocation of the HttpShell
+     *            so far at the time of invocation of this method
+     * @param defaultMeshBase the default MeshBase to use
+     * @throws HttpShellException a problem occurred, check cause for details
+     */
+    public void afterTransactionStart(
+            SaneRequest                                     request,
+            SmartFactory<MeshBase,OnDemandTransaction,Void> txs,
+            MeshBase                                        defaultMeshBase )
+        throws
+            TransactionException;
+
+    /**
+     * Invoked by the HttpShell before Transactions are closed.
+     *
+     * @param request the incoming request
+     * @param vars the variables set by the HttpShell
+     * @param txs the Transactions used by this invocation of the HttpShell
+     * @param defaultMeshBase the default MeshBase to use
+     */
+    public void beforeTransactionEnd(
+            SaneRequest                                     request,
+            Map<String,MeshObject>                          vars,
+            SmartFactory<MeshBase,OnDemandTransaction,Void> txs,
+            MeshBase                                        defaultMeshBase )
+        throws
+            TransactionException;
+
+    /**
+     * Invoked by the HttpShell after the Transactions have been closed.
      *
      * @param request the incoming request
      * @param vars the variables set by the HttpShell
      * @param txs the (now closed) Transactions used by this invocation of the HttpShell
      * @param defaultMeshBase the default MeshBase to use
+     * @param maybeThrown if a Throwable was thrown, it is passed here
      * @throws HttpShellException a problem occurred, check cause for details
      */
-    public void handle(
+    public void afterTransactionEnd(
             SaneRequest                                     request,
             Map<String,MeshObject>                          vars,
             SmartFactory<MeshBase,OnDemandTransaction,Void> txs,
-            MeshBase                                        defaultMeshBase )
+            MeshBase                                        defaultMeshBase,
+            Throwable                                       maybeThrown )
         throws
             HttpShellException;
 }
